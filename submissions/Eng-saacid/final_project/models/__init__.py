f278c6b5bd8c76b814b53ee5a56a497ba29c7bab
https://github.com/Eng-saaci <saacidsheikhcabdicaziiz@gmail.com>
2026-06-28 20:55:04 +0300

    Delete files

--- submissions/Eng-saacid/final_project/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .student import Student
-from .teacher import Teacher
-from .course import Course
--- submissions/Eng-saacid/final_project/models/course.py
+++
@@ -1,8 +0,0 @@
-
-from dataclasses import dataclass
-
-@dataclass
-class Course:
-    course_id: int
-    title: str
-    duration: int 
